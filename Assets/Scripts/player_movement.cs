2c37e27fc952a87cc9378154cae76d6c5ab62325
Lewis Inches <li10@hw.ac.uk>
2022-01-28 22:35:00 +0000

    created first C code for player movement

--- Assets/Scripts/player_movement.cs
+++ Assets/Scripts/player_movement.cs
@@ -4,6 +4,16 @@ using UnityEngine;
 
 public class player_movement : MonoBehaviour
 {
+    private CharacterController characterController;
+
+    private Vector3 direction;
+
+    public float speed = 5f;
+    
+    void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +23,17 @@ public class player_movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Movement();
+    }
+
+    void Movement()
+    {
+        direction = new Vector3(Input.GetAxis("Horizontal"), 0f, Input.GetAxis("Vertical"));
+
+        direction = transform.TransformDirection(direction);
+
+        direction *= speed * Time.deltaTime;
+
+        characterController.Move(direction);
     }
 }
